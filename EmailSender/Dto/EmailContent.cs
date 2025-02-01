ff172844324dd7d674b4fbd6e9c115600b522615
Karol <karol_maliglowka@o2.pl>
2025-02-01 21:44:29 +0100

    added field

--- EmailSender/Dto/EmailContent.cs
+++ EmailSender/Dto/EmailContent.cs
@@ -7,5 +7,6 @@ public class EmailContent
     public List<string>? Cc { get; set; }
     public List<string>? Bcc { get; set; }
     public string? Subject { get; set; }
+    public bool BodyIsHtml { get; set; }
     public string? Body { get; set; }
 }
