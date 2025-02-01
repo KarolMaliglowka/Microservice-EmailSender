255d0d5d2882be7150f0b58b6c1cfea67d48e1d9
Karol <karol_maliglowka@o2.pl>
2025-02-01 22:11:15 +0100

    changes

---
+++ EmailSender/Settings/MessageSender.cs
@@ -0,0 +1,27 @@
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+
+namespace EmailSender.Settings;
+
+public class MessageSender(IConfiguration configuration)
+{
+    public async Task MailSender(MimeMessage message)
+    {
+        var smtpSettings = new SmtpSettings();
+        configuration.GetSection(SmtpSettings.Smtp).Bind(smtpSettings);
+
+        try
+        {
+            using var client = new SmtpClient();
+            await client.ConnectAsync(smtpSettings.SmtpHost, smtpSettings.SmtpPort, smtpSettings.UseSsl);
+            await client.AuthenticateAsync(smtpSettings.EmailAddress, smtpSettings.Password);
+            await client.SendAsync(message);
+            await client.DisconnectAsync(true);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+}
